7dc3db70e0be83db0d616108caddc5ba154f6357
Will <wjv8@pitt.edu>
2025-03-19 21:37:06 -0400

    remove wrong projects

--- basketballGAME/TheGameOfBasketball/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace TheGameOfBasketball;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
