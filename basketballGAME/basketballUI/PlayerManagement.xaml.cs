5f9d9bccc2b396b4489232278c6de9913c773b45
Grout Gobbler <yad49@pitt.edu>
2025-03-25 13:54:26 -0400

    Working further.
    Added some more buttons. A little bit of formatting. Trying to allow user to enter player's team while creating them.

--- basketballGAME/basketballUI/PlayerManagement.xaml.cs
+++ basketballGAME/basketballUI/PlayerManagement.xaml.cs
@@ -6,6 +6,10 @@ namespace basketballUI;
 
 public partial class PlayerManagement : ContentPage
 {
+    string firstName = null;
+    string lastName = null;
+    String teamID = null;
+
 	public PlayerManagement()
 	{
 		InitializeComponent();
@@ -20,21 +24,58 @@ public partial class PlayerManagement : ContentPage
             try
             {
                 HttpResponseMessage response = await client.GetAsync(API_URL);
-                // List<Weather> w = JsonArray.JsonConvert.DeserializeObject<List<Person>>
+
                 List<Player> players;
+                List<TeamPlayer> playerTeams;
+
                 if (response.IsSuccessStatusCode)
                 {
                     string json = await response.Content.ReadAsStringAsync();
-                    players = JsonConvert.DeserializeObject<List<Player>>(json);
-                    Player player = new Player();
-                    Console.WriteLine("here");
+                    
+                    // Make a player.
+                        players = JsonConvert.DeserializeObject<List<Player>>(json);
 
-                    int length = players.Count;
-                    player.PlayerNo = players[length - 1].PlayerNo + 1;
-                    player.FName = "Calamity";
-                    player.LName = "Bisteria";
-                    player.TeamPlayers = new List<TeamPlayer>();
+                        Player player = new Player();
+                            int length = players.Count;
+                    
+                        // PLAYER NUMBER.
+                            player.PlayerNo = players[length - 1].PlayerNo + 1;
+
+                        // FIRST NAME.
+                            if (firstName != null)
+                                player.FName = firstName;
+                            else
+                                player.FName = "NULL";
+
+                        // LAST NAME.
+                            if (lastName != null)
+                                player.LName = lastName;
+                            else
+                                player.LName = "NULL";
+
+                    // Make a team player.
+                        playerTeams = JsonConvert.DeserializeObject<List<TeamPlayer>>(json);
+
+                        TeamPlayer playerTeam = new TeamPlayer();
+                            length = playerTeams.Count;
+
+                        // ID.
+                            playerTeam.Id = playerTeams[length - 1].Id + 1;
+
+                        // PLAYER ID.
+                            playerTeam.PlayerId = player.PlayerNo;
+
+                        // TEAM ID.
+                            if (teamID != null)
+                                playerTeam.TeamId = int.Parse(teamID);
+                            else
+                                playerTeam.TeamId = 0;
+
+                        // Assign team player to player.
+                            player.TeamPlayers = playerTeams;
+                    
                     HttpResponseMessage response1 = await client.PostAsJsonAsync(API_URL, player);
+                    
                     if (response1.IsSuccessStatusCode)
                     {
                         CreatePlayerButton.Text = $"success ";
@@ -54,15 +95,24 @@ public partial class PlayerManagement : ContentPage
                 CreatePlayerButton.Text = $"Clicked and failed " + ex;
             }
         }
+
+        firstName = null;
+        lastName = null;
+        teamID = null;
 	}
 
-    private void entry_TextChanged(object sender, TextChangedEventArgs e)
+    private void PlayerFirstNameEntry_TextChanged(object sender, TextChangedEventArgs e)
     {
-
+        firstName = e.NewTextValue;
     }
 
-    private void entry_Completed(object sender, EventArgs e)
+    private void PlayerLastNameEntry_TextChanged(object sender, TextChangedEventArgs e)
     {
+        lastName = e.NewTextValue;
+    }
 
+    private void PlayerTeamEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+            teamID = e.NewTextValue;
     }
 }
