08e35f142145e3c2cc8f468b5a997f39f8eb40ef
Will <wjv8@pitt.edu>
2025-03-26 01:01:11 -0400

    Update ScheduleManagement.xaml.cs

--- basketballGAME/basketballUI/ScheduleManagement.xaml.cs
+++ basketballGAME/basketballUI/ScheduleManagement.xaml.cs
@@ -20,7 +20,46 @@ public partial class ScheduleManagement : ContentPage
         InitializeComponent();
 	}
 
-    async private void Create_Schedule_Button_Click(object sender, EventArgs e) { }
+    async private void Create_Schedule_Button_Click(object sender, EventArgs e) {
+        string API_URL_1 = "http://localhost:5121/api/Schedules";
+        string API_URL_2 = "http://localhost:5121/api/Games";
+
+        using (HttpClient client = new HttpClient())
+        {
+            try {
+                HttpResponseMessage response1 = await client.GetAsync(API_URL_1);
+                HttpResponseMessage response2 = await client.GetAsync(API_URL_2);
+
+                List<Team> teams;
+                List<Game> games;
+
+                if (response1.IsSuccessStatusCode && response2.IsSuccessStatusCode)
+                {
+                    string json1 = await response1.Content.ReadAsStringAsync();
+                    teams = JsonConvert.DeserializeObject<List<Team>>(json1);
+
+                    string json2 = await response2.Content.ReadAsStringAsync();
+                    games = JsonConvert.DeserializeObject<List<Game>>(json2);
+
+                    Player player = new Player();
+                    int length1 = teams.Count;
+
+                    TeamPlayer teamPlayer = new TeamPlayer();
+                    int length2 = games.Count;
+
+                    player.PlayerNo = teams[length1 - 1].PlayerNo + 1;
+                    teamPlayer.Id = games[length2 - 1].Id + 1;
+                }
+                else
+                {
+                    CreateGameButton.Text = $"no success";
+                }
+            }
+            catch (Exception ex) {
+                CreateGameButton.Text = $"Clicked and failed " + ex;
+            }
+        }
+    }
 
     private void DateEntry_TextChanged(object sender, TextChangedEventArgs e)
     {
