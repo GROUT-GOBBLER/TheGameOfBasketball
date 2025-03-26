1c284a44108751f610b074fa96421c43a1693c17
Will <wjv8@pitt.edu>
2025-03-26 00:25:57 -0400

    starting

--- basketballGAME/basketballUI/ScheduleManagement.xaml.cs
+++ basketballGAME/basketballUI/ScheduleManagement.xaml.cs
@@ -1,13 +1,60 @@
 using basketballUI.models;
 using Newtonsoft.Json;
 using System.Net.Http.Json;
+using Windows.Media.AppBroadcasting;
 
 namespace basketballUI;
 
 public partial class ScheduleManagement : ContentPage
 {
+    string date;
+    string time;
+    string teamOne;
+    string teamTwo;
+    string city;
+    string state;
+    string zip;
+
 	public ScheduleManagement()
 	{
         InitializeComponent();
 	}
+
+    async private void Create_Schedule_Button_Click(object sender, EventArgs e) { }
+
+    private void DateEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        date = e.NewTextValue;
+    }
+
+    private void TimeEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        time = e.NewTextValue;
+    }
+
+    private void TeamOneEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        teamOne = e.NewTextValue;
+    }
+
+    private void TeamTwoEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        teamTwo = e.NewTextValue;
+    }
+
+    private void CityEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        city = e.NewTextValue;
+    }
+
+    private void StateEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        state = e.NewTextValue;
+    }
+
+    private void ZipEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        zip = e.NewTextValue;
+    }
+
 }
